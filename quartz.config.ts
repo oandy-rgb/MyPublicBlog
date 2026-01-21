1e78a7c5850a8678adf0ab67a9d63e2a57956c84
oandy-rgb <andy29592062@gmail.com>
2026-01-22 00:44:11 +0800

    更新社群連結與標題

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Oandy's Blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
